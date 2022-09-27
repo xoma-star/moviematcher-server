a003c5de00926e104c61ee9f72a1d2da0abf7da8
xoma_star <sasha.sirotyin09@gmail.com>
2022-09-27 21:44:25 +0500

    image compression

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,15 +1,15 @@
 import {Controller, Get, Param, Req, Res} from '@nestjs/common';
 import {Request, Response} from "express";
-import axios from "axios";
+import {ImageService} from "./image.service";
 
 @Controller('image')
 export class ImageController {
+    constructor(private imageService: ImageService) {}
     @Get(':path')
     async get(@Param('path') path: string, @Req() req: Request, @Res() res: Response){
         try {
-            const a = await axios.get(path, {responseType: "arraybuffer"})
-            res.contentType('image/jpeg')
-            res.send(a.data)
+            res.contentType('image/webp')
+            res.send(await this.imageService.get(path))
         }catch (e) {throw e}
     }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import axios from "axios";
+import * as sharp from 'sharp'
 
 @Injectable()
-export class ImageService {}
+export class ImageService {
+    async get(path: string){
+        const image = await axios.get(path, {responseType: "arraybuffer"})
+        return await sharp(image.data).toColourspace('rgb16').jpeg({quality: 10, force: false, progressive: true}).toBuffer()
+    }
+}
