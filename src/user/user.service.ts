da43c00c8f5991c8823edf8e60ec15240f0ba64a
xoma_star <sasha.sirotyin09@gmail.com>
2022-09-28 19:16:24 +0500

    fix

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -40,6 +40,10 @@ export class UserService {
         const rated = [...user.saved, ...user.skipped, ...user.liked, ...user.disliked]
         const movies = await this.movieService.getAllMovies()
         //TODO: каждый раз сортировать и фильтровать - полная хуйня. заменить.
-        return movies.sort(() => 0.5 - Math.random()).filter(x => rated.indexOf(x.id) < 0 && x.popularity > 30).slice(0, count).map(x => ({...x, screens: x.screens.slice(0, 4)}))
+        return movies
+            .sort(() => 0.5 - Math.random())
+            .filter(x => rated.indexOf(x.id) < 0 && x.popularity > 30)
+            .slice(0, count)
+            .map(x => ({...x, screens: [...x.screens].sort(() => 0.5 - Math.random()).slice(0, 4).map(v => `https://api.xoma-star.tk/image/${encodeURIComponent(v)}`)}))
     }
 }
