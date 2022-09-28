467991890b9fdb4363d1fd9f33797d00d8be9c2d
xoma_star <sasha.sirotyin09@gmail.com>
2022-09-28 18:51:19 +0500

    redis

---
+++ src/redis/redis.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@nestjs/common';
+import * as Redis from 'redis'
+
+const client = Redis.createClient({url: 'redis://45.159.249.198:6379'});
+(async () => {
+    try{
+        await client.connect()
+        console.log('Redis connected')
+    }catch (e){process.exit(1)}
+})()
+
+@Injectable()
+export class RedisService {
+    async set(field: string, value: string){
+        await client.setEx(field, 60 * 60 * 24, value)
+    }
+
+    async get(field: string) {
+        return await client.get(field)
+    }
+}
