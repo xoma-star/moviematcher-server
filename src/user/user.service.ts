271f081a70ffc4ab9228301573204ed24e2b3b79
xoma_star <sasha.sirotyin09@gmail.com>
2022-09-28 18:55:17 +0500

    redis

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -40,6 +40,6 @@ export class UserService {
         const rated = [...user.saved, ...user.skipped, ...user.liked, ...user.disliked]
         const movies = await this.movieService.getAllMovies()
         //TODO: каждый раз сортировать и фильтровать - полная хуйня. заменить.
-        return movies.sort(() => 0.5 - Math.random()).filter(x => rated.indexOf(x.id) < 0 && x.popularity > 30).slice(0, count)
+        return movies.sort(() => 0.5 - Math.random()).filter(x => rated.indexOf(x.id) < 0 && x.popularity > 30).slice(0, count).map(x => ({...x, screens: x.screens.slice(0, 4)}))
     }
 }
